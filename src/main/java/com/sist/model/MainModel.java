8047360a390926c64b2077a3188ce3259bd94ef8
necteo <89801040+necteo@users.noreply.github.com>
2025-10-31 10:17:40 +0900

    project_1

---
+++ src/main/java/com/sist/model/MainModel.java
@@ -0,0 +1,19 @@
+package com.sist.model;
+
+import com.sist.controller.Controller;
+import com.sist.controller.RequestMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Controller
+public class MainModel {
+  @RequestMapping("main/main.do")
+  public String main_main(HttpServletRequest request,
+		  HttpServletResponse response)
+  {
+	  // main_jsp => 화면 
+	  request.setAttribute("main_jsp", "../main/home.jsp");
+	  return "../main/main.jsp";
+  }
+}
